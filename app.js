cdad25a2cefe96d12c38639564dcc7559724b5b1
maaronkel <maaronkel@protonmail.com>
2020-09-21 12:28:13 +1000

    Add for loop to display every match for the week

--- app.js
+++ app.js
@@ -5,18 +5,34 @@ $.ajax({
   type: "GET",
 }).done(function (response) {
   //console.log(response);
-  let x = document.getElementById("div1");
-  let y = document.getElementById("div2");
-  x.innerHTML = response.matches[6].score.fullTime.homeTeam; // score
-  y.innerHTML = response.matches[6].score.fullTime.awayTeam; // score
-  let xName = document.createElement("div");
-  let yName = document.createElement("div");
+  //let x = document.getElementById("div1");
+  //let y = document.getElementById("div2");
+  //x.innerHTML = response.matches[6].score.fullTime.homeTeam; // score
+  //y.innerHTML = response.matches[6].score.fullTime.awayTeam; // score
+  //let xName = document.createElement("div");
+  //let yName = document.createElement("div");
   let body = document.querySelector("body");
-  body.appendChild(xName);
-  body.appendChild(yName);
-  xName.innerHTML = response.matches[6].homeTeam.name; // team name
-  yName.innerHTML = response.matches[6].awayTeam.name; // team name
+  //body.appendChild(xName);
+  //body.appendChild(yName);
+  //xName.innerHTML = response.matches[6].homeTeam.name; // team name
+  //yName.innerHTML = response.matches[6].awayTeam.name; // team name
   //response.matches.map()
   //fixtureArray = []
-  //let x = response.matches;
+  //console.log(response.matches);
+  for (let i in response.matches) {
+    let match = document.createElement("div");
+    let scoreH = document.createElement("p");
+    let scoreA = document.createElement("p");
+    let teamH = document.createElement("p");
+    let teamA = document.createElement("p");
+    teamH.textContent = response.matches[i].homeTeam.name;
+    teamA.textContent = response.matches[i].awayTeam.name;
+    scoreH.textContent = response.matches[i].score.fullTime.homeTeam;
+    scoreA.textContent = response.matches[i].score.fullTime.awayTeam;
+    body.appendChild(match);
+    match.appendChild(scoreH);
+    match.appendChild(scoreA);
+    match.appendChild(teamH);
+    match.appendChild(teamA);
+  }
 });
