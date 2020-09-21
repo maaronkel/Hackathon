80743ca93a2356f5f9a6cd623ee5782897455e31
Alex Qiang <alex.qiang36@gmail.com>
2020-09-21 15:38:12 +1000

    fetch new api and add videos to page

--- app.js
+++ app.js
@@ -1,38 +1,70 @@
-$.ajax({
-  headers: { "X-Auth-Token": "bebca4b77c9d4d58849ac24ee825106a" },
-  url: "https://api.football-data.org/v2/competitions/PL/matches?matchday=2",
-  dataType: "json",
-  type: "GET",
-}).done(function (response) {
-  //console.log(response);
-  //let x = document.getElementById("div1");
-  //let y = document.getElementById("div2");
-  //x.innerHTML = response.matches[6].score.fullTime.homeTeam; // score
-  //y.innerHTML = response.matches[6].score.fullTime.awayTeam; // score
-  //let xName = document.createElement("div");
-  //let yName = document.createElement("div");
+// $.ajax({
+//   headers: { "X-Auth-Token": "bebca4b77c9d4d58849ac24ee825106a" },
+//   url: "https://api.football-data.org/v2/competitions/PL/matches?matchday=2",
+//   dataType: "json",
+//   type: "GET",
+// }).done(function (response) {
+
+  // let body = document.querySelector("body");
+
+//   for (let i in response.matches) {
+//     let match = document.createElement("div");
+//     let video = document.createElement('div');
+//     let scoreH = document.createElement("p");
+//     let scoreA = document.createElement("p");
+//     let teamH = document.createElement("p");
+//     let teamA = document.createElement("p");
+//     teamH.textContent = response.matches[i].homeTeam.name;
+//     teamA.textContent = response.matches[i].awayTeam.name;
+//     scoreH.textContent = response.matches[i].score.fullTime.homeTeam;
+//     scoreA.textContent = response.matches[i].score.fullTime.awayTeam;
+//     body.appendChild(match);
+//     match.appendChild(video);
+//     match.appendChild(scoreH);
+//     match.appendChild(scoreA);
+//     match.appendChild(teamH);
+//     match.appendChild(teamA);
+
+//   }
+  
+// });
+
+
+
+
+let getFootballVid = () => {
+  axios
+  .get("https://www.scorebat.com/video-api/v1/")
+  .then(console.log("asdf"))
+  .then((response) => {
+  //   for (i = 0; i < 5; i++) {console.log(response.data[i].videos[0]) }
+  // // console.log(response.data[1].video)
+  fixtureVids(response)
+  })
+  .catch((err) => console.log(err));
+  };
+
+
+fixtureVids = (response) => {
+  
   let body = document.querySelector("body");
-  //body.appendChild(xName);
-  //body.appendChild(yName);
-  //xName.innerHTML = response.matches[6].homeTeam.name; // team name
-  //yName.innerHTML = response.matches[6].awayTeam.name; // team name
-  //response.matches.map()
-  //fixtureArray = []
-  //console.log(response.matches);
-  for (let i in response.matches) {
-    let match = document.createElement("div");
-    let scoreH = document.createElement("p");
-    let scoreA = document.createElement("p");
-    let teamH = document.createElement("p");
-    let teamA = document.createElement("p");
-    teamH.textContent = response.matches[i].homeTeam.name;
-    teamA.textContent = response.matches[i].awayTeam.name;
-    scoreH.textContent = response.matches[i].score.fullTime.homeTeam;
-    scoreA.textContent = response.matches[i].score.fullTime.awayTeam;
-    body.appendChild(match);
-    match.appendChild(scoreH);
-    match.appendChild(scoreA);
-    match.appendChild(teamH);
-    match.appendChild(teamA);
+
+  for (let i in response.data) {
+    if ( response.data[i].competition.id == 15) {
+      let match = document.createElement("div");
+      let video = document.createElement('div');
+      let teams = document.createElement("p");
+
+      teams.textContent = response.data[i].title;
+      video.innerHTML = response.data[i].videos[0].embed;
+
+      body.appendChild(match);
+      match.appendChild(video);
+      match.appendChild(teams);
+    }
   }
-});
+
+
+}
+
+getFootballVid()
