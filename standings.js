135899fb0079aa715a0ee2170ebdb939dc8076f5
maaronkel <maaronkel@protonmail.com>
2020-09-22 14:25:43 +1000

    add dynamic league standings

--- standings.js
+++ standings.js
@@ -1,56 +1,52 @@
-
 let table = document.querySelector("#table");
 let main = document.querySelector("main");
 main.appendChild(table);
 
 function deleteChildren() {
-    let first = table.firstElementChild;
-    while (first) {
-      first.remove();
-      first = table.firstElementChild;
-    }
+  let first = table.firstElementChild;
+  while (first) {
+    first.remove();
+    first = table.firstElementChild;
+  }
 }
 
-$.ajax({
-  headers: { "X-Auth-Token": "bebca4b77c9d4d58849ac24ee825106a" },
-  // country dynamic function returns string 
-  url: `https://api.football-data.org/v2/competitions/PL/standings`,
-  dataType: "json",
-  type: "GET",
-}).done(function (response) {
-
-    deleteChildren()    
-//     // tableData = response.standings
-  for (let i in response.standings[0].table) {
-
-    // console.log(response.standings[0].table)
-    createTable(response.standings[0].table, i)
-
-    }
-});
+let getData = (country) => {
+  axios
+    .get(`https://api.football-data.org/v2/competitions/${country}/standings`, {
+      headers: {
+        "X-Auth-Token": "bebca4b77c9d4d58849ac24ee825106a",
+      },
+    })
+    .then((response) => {
+      deleteChildren();
+      for (let i in response.data.standings[0].table) {
+        createTable(response.data.standings[0].table, i);
+      }
+    })
+    .catch((err) => console.log(err));
+};
 
 createTable = (data, i) => {
-    // console.log(response, i)
-    let team = document.createElement("ul");
-    let teamName = document.createElement("li");
-    let teamPos = document.createElement("li");
-    let teamPoints = document.createElement("li");
-    let teamCrest = document.createElement("img");
-    table.appendChild(team)
-    team.appendChild(teamPos)
-    team.appendChild(teamCrest)
-    team.appendChild(teamName)
-    team.appendChild(teamPoints)
-    teamPos.textContent = data[i].position
-    teamCrest.src = data[i].team.crestUrl
-    teamName.textContent = data[i].team.name
-    teamPoints.textContent = data[i].points
-    
-}
+  // console.log(response, i)
+  let team = document.createElement("ul");
+  let teamName = document.createElement("li");
+  let teamPos = document.createElement("li");
+  let teamPoints = document.createElement("li");
+  let teamCrest = document.createElement("img");
+  table.appendChild(team);
+  team.appendChild(teamPos);
+  team.appendChild(teamCrest);
+  team.appendChild(teamName);
+  team.appendChild(teamPoints);
+  teamPos.textContent = data[i].position;
+  teamCrest.src = data[i].team.crestUrl;
+  teamName.textContent = data[i].team.name;
+  teamPoints.textContent = data[i].points;
+};
 
+document.getElementById("eng").addEventListener("click", () => getData("PL"));
+document.getElementById("spa").addEventListener("click", () => getData("PD"));
+document.getElementById("ita").addEventListener("click", () => getData("SA"));
+document.getElementById("fra").addEventListener("click", () => getData("FL1"));
+document.getElementById("ger").addEventListener("click", () => getData("BL1"));
 
-//SA
-//PL
-//BL1
-//PD
-//FL1
